bb94cb3b685418d6c02424aca1d036ec14affcb2
muslumdogan01 <muslumdogan01@gmail.com>
2021-07-19 02:49:25 +0300

    feat: menü bar finish

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navbar() {
-  const filmLinks = [
+  const [isOpen, setOpen] = useState(false);
+
+  const onClickFunction = () => {
+    setOpen(!isOpen);
+  };
+  let filmLinks = [
     "Arşiv",
     "Film Türleri",
     "En Çok İzlnen Filmler",
@@ -12,15 +17,53 @@ function Navbar() {
     "Türkçe Dublaj Filmler",
     "Yabancı Dizi İzle",
   ];
+  let movieGenres = [
+    "4K",
+    "Aile",
+    "Aksiyon",
+    "Animasyon",
+    "Belgesel",
+    "Bilim-Kurgu",
+    "Altyazılı Filmler",
+    "Dram",
+    "Yabancı",
+    "Fantastik",
+    "Gerilim",
+    "Gizem",
+    "Komedi",
+    "Korku",
+    "Suç",
+    "Savaş",
+    "Macera",
+  ];
+
   return (
     <div className="navbar_content">
       <ul>
         <li className="navbar_content_films">
-          {filmLinks.map((film) => (
-            <div>{film}</div>
+          {filmLinks.map((film, index) => (
+            <a
+              href="#"
+              onClick={index == 1 && onClickFunction}
+              className="navbar_content_films_film"
+            >
+              {film}
+            </a>
           ))}
         </li>
       </ul>
+      <div className="navbar_content_hideMenu">
+        <ul className="navbar_content_film_türleri">
+          <li className="navbar_content_film_tür">
+            {isOpen &&
+              movieGenres.map((movies) => (
+                <a href="#" className="navbar_content_film_links">
+                  {movies}
+                </a>
+              ))}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
